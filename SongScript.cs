35fe73941a02ce30d62426b21a26a67a2155c816
SUNATARO HOSOI <fantasticbanana4t@yahoo.co.jp>
2017-10-30 13:54:00 +0900

    Update SongScript.cs

--- SongScript.cs
+++ SongScript.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SongScript : MonoBehaviour {
-
+	
+	// 歌を流す機能をつけるスクリプト
+	// 音の発信源にしたい場所（今回はケーキ本体）にアタッチした
 	private AudioSource audio;
 
-	// Use this for initialization
 	void Start () {
 		audio = gameObject.GetComponent<AudioSource>();
 
