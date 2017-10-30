f16a087f0ac980114fc12448dc767b77f1e29926
SUNATARO HOSOI <fantasticbanana4t@yahoo.co.jp>
2017-10-30 13:48:42 +0900

    Update Move.cs

--- Move.cs
+++ Move.cs
@@ -3,13 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Move : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-
-	}
-
-	// Update is called once per frame
+	
+	// １フレームごとにz方向へ−0.02ずつ移動する
+	// HAPPY BIRTHDAY の文字にアタッチしたスクリプト
 	void Update () {
 		transform.position += new Vector3 (0, 0, - 0.02f);
 	}
