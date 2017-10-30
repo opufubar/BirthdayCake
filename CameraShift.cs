e27f84a2225060653355daf1484c42d5887bdf13
SUNATARO HOSOI <fantasticbanana4t@yahoo.co.jp>
2017-10-30 14:00:53 +0900

    Update CameraShift.cs

--- CameraShift.cs
+++ CameraShift.cs
@@ -4,26 +4,36 @@ using UnityEngine;
 
 public class CameraShift : MonoBehaviour {
 
+	// ネットから引っ張ってきた
+	
 	private GameObject MainCam;
 	private GameObject SubCam;
-
+	
+	
 	void Start () {
-			MainCam = GameObject.Find("MainCamera");
-			SubCam = GameObject.Find("SubCamera");
-
-			SubCam.SetActive(false);
+		
+		// カメラ２つのGameObjectを入手して変数に代入
+		MainCam = GameObject.Find("MainCamera");
+		SubCam = GameObject.Find("SubCamera");
+	
+		// 始めの状態では２番手のカメラを非アクティブ状態にしておく
+		SubCam.SetActive(false);
 	}
 
 	void Update () {
-			if(Input.GetKeyDown("space")){
-					if(MainCam.activeSelf){
-							MainCam.SetActive (false);
-							SubCam.SetActive (true);
-					}else{
-							MainCam.SetActive (true);
-							SubCam.SetActive (false);
-					}
+		
+		// スペースキーを押すと..
+		if(Input.GetKeyDown("space")){
+			
+			// MainCamとSubCamのアクティブ状態を変更する
+			if(MainCam.activeSelf){
+				MainCam.SetActive (false);	
+				SubCam.SetActive (true);
+			}else{
+				MainCam.SetActive (true);
+				SubCam.SetActive (false);
 			}
+		}
 	}
 
 
