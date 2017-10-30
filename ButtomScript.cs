f99c66420dab4a20943eb6609af8922c6b972fcb
SUNATARO HOSOI <fantasticbanana4t@yahoo.co.jp>
2017-10-30 13:49:45 +0900

    Update ButtomScript.cs

--- ButtomScript.cs
+++ ButtomScript.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ButtomScript : MonoBehaviour {
+	
+	// 「祝ってもらう」ボタンにアタッチしたスクリプト
 	// ボタンを押すとシーンがmain2に切り替わる
 	public void SceneLoad(){
 		Application.LoadLevel("main2");
